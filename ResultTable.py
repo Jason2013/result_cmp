ab3a62dca159f2cbef2a8fbe335aeffe9ab25307
Jason Chen <shchenchang@gmail.com>
2018-12-20 17:34:44 +0800

    Read and output the sample data to the console.

--- ResultTable.py
+++ ResultTable.py
@@ -8,3 +8,38 @@ class RowState(Enum):
     ResultEnd = 4
 
 TEST_RESULTS_FILE = r"D:\work\dev\teamcity\data\shaderbench\Ariel_llpc\1\001\test_results.txt"
+
+
+class TestResult(object):
+
+    def __init__(self, TestResultFile = None):
+        self.testResultFile = TestResultFile if TestResultFile else None
+
+    def LoadData(self):
+        state = RowState.Misc
+        with open(self.testResultFile) as f:
+            for row in f:
+                row = row.strip()
+                if state == RowState.Misc:
+                    # if 
+                    if row.startswith("["):
+                        pass
+                    elif not row:
+                        state = RowState.ResultHeader
+                    else:
+                        assert False
+                elif state == RowState.ResultHeader:
+                    assert row
+                    print("Header: " + row)
+                    state = RowState.ResultData
+                elif state == RowState.ResultData:
+                    if row:
+                        print("Data: " + row)
+                    else:
+                        state = RowState.ResultHeader
+                else:
+                    assert False
+
+if __name__ == "__main__":
+    r = TestResult(TEST_RESULTS_FILE)
+    r.LoadData()
