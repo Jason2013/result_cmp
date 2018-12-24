811b7c805c45d49ead50b11dcb98aff9d3293be0
Jason Chen <shchenchang@gmail.com>
2018-12-24 17:34:32 +0800

    Add `TestResultTable.Compatible` test case.

--- MyTest.py
+++ MyTest.py
@@ -27,5 +27,14 @@ class TestResultTable(unittest.TestCase):
     def tearDown(self):
         pass
 
+    def test_compatible(self):
+        TEST_RESULTS_FILE = r"D:\work\dev\teamcity\data\shaderbench\Ariel_llpc\1\001\test_results.txt"
+        r1 = ResultTable.TestResult(TEST_RESULTS_FILE)
+        r1.LoadData()
+        r2 = ResultTable.TestResult(TEST_RESULTS_FILE)
+        r2.LoadData()
+        self.assertTrue(r1.tabs[0].Compatible(r2.tabs[0]))
+
+
 if __name__ == "__main__":
     unittest.main()
