8737b08fb7d8f537b7800b8ad878d75bf15bc72d
Jason Chen <shchenchang@gmail.com>
2018-12-24 15:25:10 +0800

    Add `MyTest` file.

---
+++ MyTest.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+
+import unittest
+import ResultTable
+
+class TestResultTable(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
