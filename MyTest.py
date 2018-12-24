7521bda40068234a24bc53a4f4833298345ea4b4
Jason Chen <shchenchang@gmail.com>
2018-12-24 15:45:40 +0800

    Add `TestColumnHeader` class.

--- MyTest.py
+++ MyTest.py
@@ -3,6 +3,22 @@
 import unittest
 import ResultTable
 
+class TestColumnHeader(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_init(self):
+        x = ResultTable.ColumnHeader("header")
+        self.assertEqual(x.caption, "header")
+        self.assertEqual(x.columnWidth, 1)
+        self.assertEqual(x.dataType, ResultTable.ColumnDataType.Number)
+        self.assertEqual(x.maxDataWidth, 1)
+
+
 class TestResultTable(unittest.TestCase):
 
     def setUp(self):
