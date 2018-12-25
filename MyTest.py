a8ff902e291a2b8804a1a5731285f41c76e3bf03
Jason Chen <shchenchang@gmail.com>
2018-12-25 15:06:33 +0800

    Add `deepcopy` test.

--- MyTest.py
+++ MyTest.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 
+import copy
 import unittest
 import pickle
 
@@ -45,12 +46,14 @@ class TestResultTable(unittest.TestCase):
         TEST_RESULTS_FILE = r"data\shaderbench\Ariel_llpc\1\001\test_results.txt"
         r1 = ResultTable.TestResult(TEST_RESULTS_FILE)
         r1.LoadData()
+
+        r2 = copy.deepcopy(r1)
         # r2 = ResultTable.TestResult(TEST_RESULTS_FILE)
         # r2.LoadData()
         # self.assertTrue(r1.tabs[0].Compatible(r2.tabs[0]))
         with open("TestResult_data_shaderbench_Ariel_llpc_1_001_test_results.txt", "rb") as f:
             r3 = pickle.load(f)
-            self.assertTrue(r1.tabs[0].Compatible(r3.tabs[0]))
+            self.assertTrue(r2.tabs[0].Compatible(r3.tabs[0]))
 
 
 if __name__ == "__main__":
